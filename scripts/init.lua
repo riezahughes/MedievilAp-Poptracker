162aa47b41d287cde92498b5b109cad754521ad7
Kuzurreesh <nino-077@hotmail.com>
2025-08-19 12:27:35 +0200

    Update init.lua

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,11 +1,13 @@
-
 ScriptHost:LoadScript("scripts/autotracking.lua")
+ScriptHost:LoadScript("scripts/locations.lua")
+
 Tracker:AddItems("items/items.json")
 
 Tracker:AddMaps("maps/maps.json")
 
 Tracker:AddLayouts("layouts/item_grids.json")
 Tracker:AddLayouts("layouts/layouts.json")
+
 
 
 

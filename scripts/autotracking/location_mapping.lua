eb382d70031ce2631059e7fbc2a68c0388cc6d7d
Kuzurreesh <nino-077@hotmail.com>
2025-08-19 12:41:47 +0200

    Update location_mapping.lua

---
+++ scripts/autotracking/location_mapping.lua
@@ -0,0 +1,29 @@
+LOCATION_MAPPING = {
+	-- Dan's Crypt
+	[99252000] = {"@Dan's Crypt/Corridor/Star Rune"},
+	[99252001] = {"@Dan's Crypt/Mound/Life Bottle"},
+	[99252002] = {"@Dan's Crypt/Right There/Small Sword"},
+	[99252003] = {"@Dan's Crypt/End of Corridor/Copper Shield"},
+	[99252004] = {"@Dan's Crypt/Locked Gate/Throwing Daggers"},
+	[99252005] = {"@Dan's Crypt/End of Corridor/Coin Bag"},
+	[99252006] = {"@Dan's Crypt/Crypt Clear/cleared crypt"},
+	[99274000] = {"@Dan's Crypt/Behind Wall/Life Bottle"},
+	[99274001] = {"@Dan's Crypt/Behind Wall/Coin Chest Left"},
+	[99274002] = {"@Dan's Crypt/Behind Wall/Coin Chest Right"},
+	
+	--The Graveyard
+	[99253000] = {"@The Graveyard/Life Bottle/Life Bottle"},
+	[99253001] = {"@The Graveyard/First Crypt/Earth Rune"},
+	[99253002] = {"@The Graveyard/Past Earth Gate/Chaos Rune"},
+	[99253003] = {"@The Graveyard/Island/Copper Shield"},
+	[99253004] = {"@The Graveyard/Entrance/Coin Bag"},
+	[99253005] = {"@The Graveyard/Past Earth Gate/Coin Bag"},
+	[99253006] = {"@The Graveyard/Behind Fence/Coin Chest"},
+	[99253007] = {"@The Graveyard/Life Bottle/Coin Chest Left"},
+	[99253008] = {"@The Graveyard/Life Bottle/Coin Chest Right"},
+	[99253009] = {"@The Graveyard/Shop/Coin Chest"},
+	[99253010] = {"@The Graveyard/Fountain Near End/Coin Bag"},
+	[99253011] = {"@The Graveyard/Graveyard Clear"},
+	[99253012] = {"@The Graveyard/Spinning Statue/Chalice"}
+
+}
